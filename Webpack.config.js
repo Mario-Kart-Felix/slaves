c628d37566f90be1b9c707f1e01f6036bd89771b
Mario-Kart-Felix <76971465+Mario-Kart-Felix@users.noreply.github.com>
2021-06-22 05:57:19 -0500

    Create Webpack.config.js

---
+++ Webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = (env) => ({
+  mode: !!env.production ? 'production' : 'development',
+  entry: './src/index.tsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[contenthash].js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: 'ts-loader',
+      },
+      {
+        test: /(\.gz|\.jpg|tesseract-core\.wasm\.js|worker\.min\.js|\.woff2)$/i,
+        loader: 'file-loader',
+        options: {
+          name: 'lib/[name].[ext]',
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.ts', '.tsx'],
+  },
+  plugins: [new HtmlWebpackPlugin({ template: 'src/index.html' })],
+  devServer: {
+    port: 1234,
+    historyApiFallback: true,
+    useLocalIp: true,
+    http2: true,
+    // Accessing camera requires HTTPS
+    https: true,
+  },
+})
